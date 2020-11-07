9580ed212f3649a2031f02a1ce40338b8c7de842
unknown <mohammadhaq01@gmail.com>
2020-11-07 06:51:10 +0100

    updates

--- cars/spiders/cars01.py
+++ cars/spiders/cars01.py
@@ -1,6 +1,4 @@
 import scrapy
-import pandas as pd
-
 
 
 chrome_path = r"C:\Users\haqab\Desktop\DS\chromedriver.exe"
@@ -8,28 +6,73 @@ chrome_path = r"C:\Users\haqab\Desktop\DS\chromedriver.exe"
 class Cars01Spider(scrapy.Spider):
     name = 'cars01'
     #allowed_domains = ['https://www.olx.in/']
-    start_urls = ['https://www.olx.in/en/item/toyota-etios-vd-2012-diesel-iid-1595588005']
+    start_urls = ['https://www.olx.in/cars_c84']
     
-    # def parse(self,response):
-    #     df = pd.read_csv(r'C:\Users\haqab\Desktop\DS\cars\output_cars.csv')
-    #     prices_price = df['URL'].tolist()
-    #     for i in prices_price:
-    #         #print(prices_price)
-    #         yield scrapy.Request(str(i), callback = self.parselevel1)
+    def __init__(self):
+        self.driver = webdriver.Chrome(chrome_path)
+    
+    def parse(self,response):
+        cities_ = response.xpath('//*[@class="_396bV _2zs4P _1nXYG"]').extract()
+        for city in cities_:     
+            i = city.xpath('//@href').extract_first()
+            yield scrapy.Request(str(city), callback = self.parselevel1)
+            
+    def parselevel1(self, response):
+        for j in range(1, 15):
+            try:
+            time.sleep(1)
+            # for clicking load more botton
+            load_more = self.driver.find_element_by_xpath('//*[@class="rui-3sH3b rui-3K5JC rui-1zK8h"]')
+            load_more.click()
+            
+            # escaping errors
+        except NoSuchElementException:
+            pass
         
-    def parse(self, response):
-
-        name = response.xpath('//*[@class = "_3rJ6e"]/text()').extract_first()
-        manufacture = response.xpath('//*[@class = "_18gRm"]/text()').extract_first()
-        location = response.xpath('//*[@class = "_2FRXm"]/text()').extract_first()
-        price = response.xpath('//*[@class = "_2xKfz"]/text()').extract_first()
+    # car links
+    # main links of cars to scrape.
+    posts = self.driver.find_elements_by_xpath('//*[@class = "EIR5N"]/a')
+    links = [elem.get_attribute('href') for elem in posts]
+    
+    # checking the output
+    #print(len(links)) # len of the output links.. 
+    for i_ in links:
+        yield scrapy.Request(str(i_), callback = self.parselevel2)     
+            
+    def parselevel2(self, response):
         
+        try:
+            url = self.driver.find_element_by_xpath('//*[@rel = "alternate"]')
+            URL = url.get_attribute('href')
+            #URL.append(url.get_attribute('href'))
+            name = self.driver.find_element_by_xpath('//*[@data-aut-id = "value_make"]').text
+            price = self.driver.find_element_by_xpath('//*[@class="_18gRm"]').text
+            local = self.driver.find_element_by_xpath('//*[@class="_2FRXm"]').text
+            model = self.driver.find_element_by_xpath('//*[@data-aut-id = "value_model"]').text 
+            variant = self.driver.find_element_by_xpath('//*[@data-aut-id = "value_variant"]').text
+            year = self.driver.find_element_by_xpath('//*[@data-aut-id = "value_year"]').text 
+            fuel = self.driver.find_element_by_xpath('//*[@data-aut-id = "value_petrol"]').text
+            transmission = self.driver.find_element_by_xpath('//*[@data-aut-id = "value_transmission"]').text
+            mileage = self.driver.find_element_by_xpath('//*[@data-aut-id = "value_mileage"]').text 
+            no_owner = self.driver.find_element_by_xpath('//*[@data-aut-id = "value_first_owner"]').text
 
+
+        except NoSuchElementException:
+            pass
+        
+        
         yield {
             
-            'URL' : response.url, 
+            'URL' : URL, 
             'Company_name' : name,
             'Price' : price,
-            'Location' : location,
-            'Manufacture' : manufacture
+            'Location' : local,
+            'Model' : model, 
+            'Variant' : variant, 
+            'Year' : year, 
+            'Fuel' : fuel, 
+            'Transmission' : transmission, 
+            'Mileage' : mileage, 
+            'No_owner' : no_owner, 
+            
         }
